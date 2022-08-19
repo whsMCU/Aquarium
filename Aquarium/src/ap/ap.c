5140b8102cf50970cc6daba5ad68d1f58df984b2
whsMCU <wang8763@naver.com>
2022-08-19 17:11:38 +0900

    modify for menu and mode

--- Aquarium/src/ap/ap.c
+++ Aquarium/src/ap/ap.c
@@ -19,16 +19,35 @@ typedef struct
   button_obj_t btn_enter;
   button_obj_t btn_exit;
 
+  bool menu_run;
+
   uint32_t pre_time;
 } menu_t;
 
+typedef struct
+{
+  float ds18b20_temp;
+  uint32_t sonar_distance;
+  float tds_quality;
+  float ds18b20_temp_setting;
+  uint32_t sonar_distance_setting;
+  float tds_quality_setting;
+} sensor_t;
+
 enum Menu_List
 {
+  Auto,
   Light,
   Fan,
   Pump,
   Heater,
   Setting
+};
+
+enum Mode
+{
+  Auto_Mode,
+  Menual_Mode
 };
 
 void menuInit(void);
@@ -37,11 +56,15 @@ void menuRunApp(uint8_t index);
 
 void sensorMain(void);
 
+void AutoMain(void);
+void SettingMain(void);
+
 void cliBoot(cli_args_t *args);
 
-uint8_t button_status;
+bool Mode;
 
 menu_t menu;
+sensor_t sensor;
 
 void apInit(void)
 {
@@ -50,6 +73,10 @@ void apInit(void)
 	//menuSetCallBack(lcdMain);
 	cliAdd("boot", cliBoot);
 	menuInit();
+	Mode = Menual_Mode;
+	sensor.ds18b20_temp_setting = 25.0;
+	sensor.sonar_distance_setting = 30;
+	sensor.tds_quality_setting = 10.0;
 }
 
 void apMain(void)
@@ -77,21 +104,21 @@ void sensorMain(void)
     Ds18b20_ManualConvert();
     Sonar_measure();
     tds_measure();
+    sensor.ds18b20_temp = ds18b20[0].Temperature;
+	sensor.sonar_distance = sonar_tbl[0].filter_distance_cm/10;
+	sensor.tds_quality = tds_tbl[0].filter_tdsValue;
 }
 
 void menuInit(void)
 {
-  menu.menu_cnt = 5;
+  menu.menu_cnt = 6;
   menu.menu_index = 0;
+  menu.menu_run = false;
   buttonObjCreate(&menu.btn_reset,  0, 50, 1000, 100);
   buttonObjCreate(&menu.btn_left,   1, 50, 1000, 100);
   buttonObjCreate(&menu.btn_right,  2, 50, 1000, 100);
   buttonObjCreate(&menu.btn_enter,  3, 50, 1000, 100);
   buttonObjCreate(&menu.btn_exit,   4, 50, 1000, 100);
-
-//  menu.img[0] = lcdCreateImage(&menu_img,  0, 0, 64, 80);
-//  menu.img[1] = lcdCreateImage(&menu_img, 64, 0, 64, 80);
-//  menu.img[2] = lcdCreateImage(&menu_img,128, 0, 64, 80);
 
   //buzzerSetVolume(1);
   //menuLoadInfo();
@@ -105,33 +132,12 @@ void menuUpdate(void)
   buttonObjClearAndUpdate(&menu.btn_enter);
   buttonObjClearAndUpdate(&menu.btn_exit);
 
-
-  if (buttonObjGetEvent(&menu.btn_left) & BUTTON_EVT_CLICKED)
+  if(menu.menu_run == false)
   {
-    //buzzerBeep(50);
-    if (menu.menu_index > 0)
-    {
-      menu.menu_index--;
-    }
-    else
-    {
-      menu.menu_index = menu.menu_cnt - 1;
-    }
-    menu.pre_time = millis();
-  }
-  if (buttonObjGetEvent(&menu.btn_right) & BUTTON_EVT_CLICKED)
-  {
-    //buzzerBeep(50);
-    menu.menu_index++;
-    menu.menu_index %= menu.menu_cnt;
-
-    menu.pre_time = millis();
-  }
-
-  if (buttonObjGetEvent(&menu.btn_enter) & BUTTON_EVT_CLICKED)
-  {
-    //buzzerBeep(50);
-    menuRunApp(menu.menu_index);
+	  if (buttonObjGetEvent(&menu.btn_enter) & BUTTON_EVT_CLICKED)
+	  {
+		menu.menu_run = true;
+	  }
   }
 
   static bool blink = 0;
@@ -143,34 +149,63 @@ void menuUpdate(void)
     lcdPrintf(0,16*0, green, "[삼둥이 아쿠아리움!]");
 
     lcdSetFont(LCD_FONT_HAN);
-    lcdPrintf(0,16*1, white, "물온도 : %3.1f 도" , ds18b20[0].Temperature);
-    lcdPrintf(0,16*2, white, "물높이 : %3d cm" , sonar_tbl[0].filter_distance_cm/10);
-    lcdPrintf(0,16*3, white, "TDS : %4.1f ppm" , tds_tbl[0].filter_tdsValue);
+    lcdPrintf(0,16*1, white, "물온도 : %3.1f 도" , sensor.ds18b20_temp);
+    lcdPrintf(0,16*2, white, "물높이 : %3d cm" , sensor.sonar_distance);
+    lcdPrintf(0,16*3, white, "TDS : %4.1f ppm" , sensor.tds_quality);
     //lcdDrawBufferImage(50, 20, 50, 50, TEST);
 
     blink = get_blink();
     draw_fan_status(0, 16*4, blink);
 
     lcdDrawRoundRect(0, 0+110,  35, 17, 5, white);
-		lcdDrawFillRoundRect(1, 1+110, 33, 15, 5, red);
-		lcdSetFont(LCD_FONT_07x10);
-		lcdPrintf(2,5+110, white, "Light");
+	lcdDrawFillRoundRect(1, 1+110, 33, 15, 5, red);
+	lcdSetFont(LCD_FONT_07x10);
+	lcdPrintf(2,5+110, white, "Light");
 
-		lcdDrawRoundRect(0+40, 0+110,  35, 17, 5, white);
-		lcdDrawFillRoundRect(1+40, 1+110, 33, 15, 5, red);
-		lcdSetFont(LCD_FONT_07x10);
-		lcdPrintf(5+40,5+110, white, "FAN");
+	lcdDrawRoundRect(0+40, 0+110,  35, 17, 5, white);
+	lcdDrawFillRoundRect(1+40, 1+110, 33, 15, 5, red);
+	lcdSetFont(LCD_FONT_07x10);
+	lcdPrintf(5+40,5+110, white, "FAN");
 
-		lcdDrawRoundRect(0+80, 0+110,  35, 17, 5, white);
-		lcdDrawFillRoundRect(1+80, 1+110, 33, 15, 5, red);
-		lcdSetFont(LCD_FONT_07x10);
-		lcdPrintf(5+80,5+110, white, "PUMP");
+	lcdDrawRoundRect(0+80, 0+110,  35, 17, 5, white);
+	lcdDrawFillRoundRect(1+80, 1+110, 33, 15, 5, red);
+	lcdSetFont(LCD_FONT_07x10);
+	lcdPrintf(5+80,5+110, white, "PUMP");
 
-		lcdDrawRoundRect(0+120, 0+110,  35, 17, 5, white);
-		lcdDrawFillRoundRect(1+120, 1+110, 33, 15, 5, red);
-		lcdSetFont(LCD_FONT_07x10);
-		lcdPrintf(5+120,5+110, white, "HTR");
+	lcdDrawRoundRect(0+120, 0+110,  35, 17, 5, white);
+	lcdDrawFillRoundRect(1+120, 1+110, 33, 15, 5, red);
+	lcdSetFont(LCD_FONT_07x10);
+	lcdPrintf(5+120,5+110, white, "HTR");
 
+	if(menu.menu_run == true)
+	{
+	  if (buttonObjGetEvent(&menu.btn_left) & BUTTON_EVT_CLICKED)
+	  {
+		if (menu.menu_index > 0)
+		{
+		  menu.menu_index--;
+		}
+		else
+		{
+		  menu.menu_index = menu.menu_cnt - 1;
+		}
+		menu.pre_time = millis();
+	  }
+	  if (buttonObjGetEvent(&menu.btn_right) & BUTTON_EVT_CLICKED)
+	  {
+		menu.menu_index++;
+		menu.menu_index %= menu.menu_cnt;
+
+		menu.pre_time = millis();
+	  }
+	  if (buttonObjGetEvent(&menu.btn_enter) & BUTTON_EVT_CLICKED)
+	  {
+		menuRunApp(menu.menu_index);
+	  }
+	  if (buttonObjGetEvent(&menu.btn_exit) & BUTTON_EVT_CLICKED)
+	  {
+		menu.menu_run = false;
+	  }
 		for (int i=0; i<menu.menu_cnt; i++)
 		{
 			if (menu.menu_index == Light)
@@ -198,8 +233,10 @@ void menuUpdate(void)
 				lcdPrintf(5+120,5+110, white, "HTR");
 			}
 		}
-		lcdRequestDraw();
-		}
+
+	}
+	lcdRequestDraw();
+	}
 }
 
 void menuRunApp(uint8_t index)
@@ -208,6 +245,9 @@ void menuRunApp(uint8_t index)
 
   switch(index)
   {
+  	case Auto:
+  	    AutoMain();
+  	  break;
     case Light:
     	gpioPinToggle(Relay1);
       break;
@@ -222,7 +262,7 @@ void menuRunApp(uint8_t index)
       break;
     case Setting:
     	gpioPinToggle(BUZZER);
-    	//settingMain();
+    	SettingMain();
       break;
 
     default:
@@ -232,12 +272,36 @@ void menuRunApp(uint8_t index)
 
   if (is_run == true)
   {
-	  buttonObjInit(&menu.btn_reset);
+	buttonObjInit(&menu.btn_reset);
     buttonObjInit(&menu.btn_left);
     buttonObjInit(&menu.btn_right);
     buttonObjInit(&menu.btn_enter);
     buttonObjInit(&menu.btn_exit);
   }
+}
+
+void AutoMain(void)
+{
+  uint32_t pre_time;
+
+  pre_time = millis();
+  while(1)
+  {
+	if (millis()-pre_time >= 1000)
+	{
+	  pre_time = millis();
+
+	}
+	sensorMain();
+	menuUpdate();
+	SerialCom();
+  }
+
+}
+
+void SettingMain(void)
+{
+
 }
 
 void cliBoot(cli_args_t *args)
